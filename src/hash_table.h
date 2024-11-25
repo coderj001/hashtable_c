8b6e89b9dfa28e3ba4f4a7e0e262d59e7192c6d6
Raju Ghorai <rajughorai41410@gmail.com>
2024-11-25 19:09:19 +0530

    updated my hash_table header file

--- src/hash_table.h
+++ src/hash_table.h
@@ -4,23 +4,25 @@
 #define HASH_TABLE_H
 
 typedef struct {
-    char *key;
-    char *value;
+  char *key;
+  char *value;
 } ht_item;
 
 typedef struct {
-    int size;
-    int count;
-    ht_item** items;
+  int size;
+  int count;
+  ht_item **items;
 } ht_hash_table;
 
-ht_hash_table* ht_new();
+ht_hash_table *ht_new();
 
-void ht_del_hash_table(ht_hash_table* ht);
+void ht_del_hash_table(ht_hash_table *ht);
 
-void ht_insert(ht_hash_table* ht, const char* key, const char* value);
-char* ht_search(ht_hash_table* ht, const char* key);
-void ht_delete(ht_hash_table* h, const char* key);
-void ht_print(ht_hash_table* ht);
+void ht_insert(ht_hash_table *ht, const char *key, const char *value);
+char *ht_search(ht_hash_table *ht, const char *key);
+void ht_delete(ht_hash_table *h, const char *key);
+
+// Print the hash table
+void ht_print(ht_hash_table *ht);
 
 #endif // HASH_TABLE_H
